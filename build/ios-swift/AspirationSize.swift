187f9a4121365e4553d5ebc4e554c4aee279e15b
Ryan Graves <get@ryangrav.es>
2020-02-18 15:46:05 -0700

    Initial commit

---
+++ build/ios-swift/AspirationSize.swift
@@ -0,0 +1,48 @@
+
+//
+// AspirationSize.swift
+//
+// Do not edit directly
+// Generated on Tue, 18 Feb 2020 21:44:48 GMT
+//
+
+import UIKit
+
+public enum AspirationSize {
+    public static let buttonPrimaryHorizontalPadding = CGFloat(32.00)
+    public static let buttonPrimaryVerticalPadding = CGFloat(12.00)
+    public static let buttonSecondaryHorizontalPadding = CGFloat(32.00)
+    public static let buttonSecondaryVerticalPadding = CGFloat(12.00)
+    public static let buttonTertiaryHorizontalPadding = CGFloat(24.00)
+    public static let buttonTertiaryVerticalPadding = CGFloat(4.00)
+    public static let fontBronto = CGFloat(44.00)
+    public static let fontGiga = CGFloat(20.00)
+    public static let fontHecto = CGFloat(14.00)
+    public static let fontKilo = CGFloat(16.00)
+    public static let fontKiloDigits = CGFloat(18.00)
+    public static let fontLineHeightBronto = CGFloat(44.00)
+    public static let fontLineHeightGiga = CGFloat(28.00)
+    public static let fontLineHeightHecto = CGFloat(20.00)
+    public static let fontLineHeightKilo = CGFloat(24.00)
+    public static let fontLineHeightKiloDigits = CGFloat(24.00)
+    public static let fontLineHeightMicro = CGFloat(12.00)
+    public static let fontLineHeightMini = CGFloat(14.00)
+    public static let fontLineHeightTera = CGFloat(32.00)
+    public static let fontLineHeightUno = CGFloat(16.00)
+    public static let fontLineHeightZetta = CGFloat(40.00)
+    public static let fontMicro = CGFloat(10.00)
+    public static let fontMini = CGFloat(10.92)
+    public static let fontTera = CGFloat(24.00)
+    public static let fontUno = CGFloat(12.00)
+    public static let fontZetta = CGFloat(32.00)
+    public static let spacingColossal = CGFloat(64.00)
+    public static let spacingComfy = CGFloat(24.00)
+    public static let spacingExtraNormal = CGFloat(20.00)
+    public static let spacingExtratight = CGFloat(4.00)
+    public static let spacingGiant = CGFloat(48.00)
+    public static let spacingMicro = CGFloat(2.00)
+    public static let spacingNormal = CGFloat(16.00)
+    public static let spacingSpacious = CGFloat(32.00)
+    public static let spacingTight = CGFloat(8.00)
+    public static let spacingTightish = CGFloat(12.00)
+}
